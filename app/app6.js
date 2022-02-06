e06751e1b5229e52ef16a93cdb9d3afd15e9aa4b
Yanggi Song <didrl741@gmail.com>
2022-02-06 23:56:05 +0900

    프런트요소 JS파일 만들었다.
    ejs파일에서 연결해주었다. 이를 위해서 app.js에서 app.use(express.static(`${__dirname}/src/public`)); 를 입력해 미들웨어를 등록했다.
    결과적으로, ejs에서 /js 경로를 찾을때 src/public으로 접근하게 된다.

--- app/app6.js
+++ app/app6.js
@@ -11,6 +11,10 @@ const home = require("./src/routes/home2");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+// __dirname: 현재위치 반환.
+// src 안에 public이라는 폴더를 정적경로로 추가해주겠다는 의미.
+// 이러면 login.ejs에서 /js를 보면 public의 js폴더로 접근하게 됨.
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);
 
