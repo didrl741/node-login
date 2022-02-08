4c929a3a4811ac61935275a0c0cef003e877f3aa
Yanggi Song <didrl741@gmail.com>
2022-02-08 16:28:24 +0900

    프론트단 기능 구현.
    아이디와 비번 입력하고 로그인 버튼 누르면 서버로 전송되게 하자.
    DOM: html의 요소를 JS에서 제어할 수 있게 함.
    문제점: click시 콘솔에 출력이 잠깐 됐다가 바로 사라짐.

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,19 @@
 "use strict"
+// 프론트단 기능 구현.
+// 아이디와 비번 입력하고 로그인 버튼 누르면 서버로 전송되게 하자.
+// DOM: html의 요소를 JS에서 제어할 수 있게 함.
 
-console.log("hello");
+const id = document.querySelector(".id");
+const pw = document.querySelector(".pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: pw.value,
+    };
+
+    console.log(req);
+};
